dd3f546dfae6e771404a5d45f247933a7573b2f3
asmit759 <asmitsahu759@gmail.com>
2025-08-24 15:33:11 +0530

    comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,30 @@
-import './App.css'
-import Navbar from '../components/Navbar'
-import Home from '../components/Home'
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-
-// Layout component to keep Navbar visible on all routes
-function Layout() {
-  return (
-    <div className="h-screen w-full">
-      <Navbar />
-      <Outlet /> {/* renders the matched child route */}
-    </div>
-  );
-}
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from '../components/Navbar';
+import Home from '../components/Home';
+import Register from "../components/Register";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />, // Always renders Navbar
-      children: [
-        {
-          path: "home",
-          element: <Home />,
-        },
-        // add more routes here later
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <div className="h-screen w-full">
+        <Routes>
+          {/* Home route with Navbar */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Home />
+              </>
+            }
+          />
+          <Route path="/signup" element={<Register/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
