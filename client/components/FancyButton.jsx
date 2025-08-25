832802262833c94c40d702e61cce73bb2262fe9a
Aniketdhar0810 <https://github.com/Aniketdhar810/Animan>
2025-08-25 15:29:43 +0530

    dd

--- client/components/FancyButton.jsx
+++ client/components/FancyButton.jsx
@@ -4,18 +4,14 @@ const FancyButton = ({
   label, 
   color = "#6366F1", 
   text = "white", 
-  backgroundColor = "black" 
+  backgroundColor = "black",
+  // forward any other props (onClick, type, disabled, className overrides, etc.)
+  ...rest
 }) => {
   return (
     <button
-      className="
-        relative overflow-hidden group
-        rounded-full
-        font-bold uppercase
-        px-10 py-3 tracking-wide
-        transition-transform duration-300 ease-in-out
-        hover:scale-110
-      "
+      {...rest}
+      className={"\n        relative overflow-hidden group\n        rounded-full\n        font-bold uppercase\n        px-10 py-3 tracking-wide\n        transition-transform duration-300 ease-in-out\n        hover:scale-110\n      "}
       style={{ 
         backgroundColor: backgroundColor, 
         border: `1px solid ${color}`, 
@@ -27,14 +23,7 @@ const FancyButton = ({
 
       {/* Expanding background */}
       <span
-        className="
-          absolute top-1/2 left-1/2 
-          h-0 w-0 
-          -translate-x-1/2 -translate-y-1/2 
-          rounded-full 
-          transition-all duration-300 ease-in-out 
-          group-hover:h-[500px] group-hover:w-[500px]
-        "
+        className={"\n          absolute top-1/2 left-1/2 \n          h-0 w-0 \n          -translate-x-1/2 -translate-y-1/2 \n          rounded-full \n          transition-all duration-300 ease-in-out \n          group-hover:h-[500px] group-hover:w-[500px]\n        "}
         style={{ backgroundColor: color }}
       ></span>
     </button>
