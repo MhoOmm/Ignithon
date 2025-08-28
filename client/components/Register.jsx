76c1a9ab4df4a808a797b7b1765df288bda2ec30
Mho <ommtripathi046@gmail.com>
2025-08-28 23:08:22 +0530

    axiosfix

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
+import axiosInstance from "../src/axiosConfig";
 import { useNavigate } from "react-router-dom";
 import FancyButton from "./FancyButton";
 import Logo from "../src/assets/Logo.png";
